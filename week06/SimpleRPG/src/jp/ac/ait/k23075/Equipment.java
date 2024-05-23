d2b111c57d39e11682e47fb45454ac185a52b9a1
satooru65536 <ryuto4619@gmail.com>
2024-05-23 14:10:15 +0900

    wip

---
+++ week06/SimpleRPG/src/jp/ac/ait/k23075/Equipment.java
@@ -0,0 +1,21 @@
+package jp.ac.ait.k23075;
+
+public class Equipment {
+    private String name;
+
+    public Equipment(String name) {
+        this.name = name;
+    }
+
+    public Equipment() {
+        this.name = "装備";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
